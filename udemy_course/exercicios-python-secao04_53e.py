6adca3a8479be55a3f1705be228e59da4c405868
Marcelo Caldas <mcl@DESKTOP-GVFPODK.localdomain>
2023-01-31 12:59:20 -0300

    Add update exercises Page_01

---
+++ udemy_course/exercicios-python-secao04_53e.py
@@ -0,0 +1,24 @@
+#Exercicios de input e output, python basico. 
+#Input and output exercises, basic python.
+ 
+
+"""#Write a program that reads an integer and prints it.
+inter_num = int(input('Enter a integer number: '))
+print('Your number: ', inter_num)
+print('Data type of number: ', type(inter_num))
+
+
+#Make a program that reads a real number and prints it.
+real_num = float(input('Enter a real number: '))
+print('Your number: ', real_num)
+print('Data type of number: ', type(real_num))
+
+
+#Peça ao usuário, para digitar três valores inteiros e imprima a soma deles. 
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = int(input("Enter third integer: "))
+print("The sum is", a + b + c)"""
+
+
+
